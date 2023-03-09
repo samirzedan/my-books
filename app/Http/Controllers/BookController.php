a509f27a1f83913e13f554eb6e76a9c784825114
samirzedan <samir.tzedan@gmail.com>
2023-03-09 18:52:22 -0300

    Feat: apiResource

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -24,6 +24,22 @@ class BookController extends Controller
         }
     }
 
+    public function show($id)
+    {
+        try {
+            $book = Book::findOrFail($id);
+
+            return response()->json([
+                'message' => 'Book retrieved successfully',
+                'data' => $book,
+            ]);
+        } catch (Exception $e) {
+            return response()->json([
+                'message' => $e->getMessage(),
+            ], 500);
+        }
+    }
+
     public function store(Request $request)
     {
         try {
@@ -43,6 +59,52 @@ class BookController extends Controller
 
             return response()->json([
                 'message' => 'Book created successfully',
+                'data' => $book,
+            ]);
+        } catch (Exception $e) {
+            return response()->json([
+                'message' => $e->getMessage(),
+            ], 500);
+        }
+    }
+
+    public function update(Request $request, $id)
+    {
+        try {
+            $this->validate($request, [
+                'title' => 'required|string',
+                'author' => 'required|string',
+                'total_pages' => 'required|numeric|integer|min:1',
+            ]);
+
+            $book = Book::findOrFail($id);
+
+            $book->update([
+                'title' => $request->title,
+                'author' => $request->author,
+                'total_pages' => $request->total_pages,
+            ]);
+
+            return response()->json([
+                'message' => 'Book updated successfully',
+                'data' => $book,
+            ]);
+        } catch (Exception $e) {
+            return response()->json([
+                'message' => $e->getMessage(),
+            ], 500);
+        }
+    }
+
+    public function destroy($id)
+    {
+        try {
+            $book = Book::findOrFail($id);
+
+            $book->delete();
+
+            return response()->json([
+                'message' => 'Book deleted successfully',
                 'data' => $book,
             ]);
         } catch (Exception $e) {
--- routes/api.php
+++ routes/api.php
@@ -15,4 +15,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::apiResource('books', BookController::class)->only('index', 'store');
+Route::apiResource('books', BookController::class);//->only('index', 'show', 'store', 'update', 'destroy');
